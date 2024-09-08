6fa43de410b6ff208a31b4dda3f1e58bb74b87cc
SeokSangHyun <punch9494@gmail.com>
2024-09-08 20:05:43 +0900

    first commit
    test

---
+++ Project Rythm/Assets/Script/cs_Node.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewEmptyCSharpScript
+{
+    // 버튼 정보
+    //      1. 버튼이 참조해야하는 프리팹
+    //      2. 생성 후 대기 시간
+    //      3. 노드를 생성할 위치 (테두리 인덱스) --원은 없다. 면이 많을 뿐
+    //      4. 노드 생성할 비트 혹은 시점
+
+    private Button      prefBut;
+    private float       fDeleteDelay;
+    private int         iBorder;
+    private int         iBitTime;
+
+
+
+    public void CreateNode()
+    {
+
+    }
+
+
+}
